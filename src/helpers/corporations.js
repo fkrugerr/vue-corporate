0de6e53cb689a6a268d7f24525539ea0a21b3d62
fkrugerr <fkalinin@gmail.com>
2020-01-08 17:18:52 +0200

    Fix

--- src/helpers/corporations.js
+++ src/helpers/corporations.js
@@ -1,7 +1,8 @@
 export const generateCode = () => (
   (Math.random()
     .toString(36)
-    .substring(2, 5) + Math.random().toString(36).substring(2, 5)).toUpperCase()
+    .substring(2, 5) + Math.random().toString(36).substring(2, 5)
+  ).toUpperCase()
 );
 
 export const func = val => +val;
